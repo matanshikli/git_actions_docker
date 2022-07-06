a1e1535ee8bdf9df84ff7aea2cb1b6605a7c5222
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-07 00:06:22 +0300

    update yml

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -12,11 +12,11 @@ done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+CURRENT_VERSION=$(git describe --abbrev=0 --tags 2>/dev/null)
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='v1'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
@@ -25,30 +25,18 @@ CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
-VNUM2=${CURRENT_VERSION_PARTS[1]}
-VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]
-then
+
   VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
-then
-  VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
-then
-  VNUM3=$((VNUM3+1))
-else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-  exit 1
-fi
+
 
 # create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+NEW_TAG="$VNUM1"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+GIT_COMMIT=$(git rev-parse HEAD)
+NEEDS_TAG=$(git describe --contains $GIT_COMMIT 2>/dev/null)
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
@@ -62,4 +50,4 @@ fi
 
 echo ::set-output name=git-tag::$NEW_TAG
 
-exit 0
+exit 
