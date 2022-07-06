7933de6e28570ef9865a7ddba7cf01cd55e17bf8
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-07 01:10:08 +0300

    LAST

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -16,7 +16,7 @@ CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='v1'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
@@ -25,25 +25,12 @@ CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
-VNUM2=${CURRENT_VERSION_PARTS[1]}
-VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]
-then
   VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
-then
-  VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
-then
-  VNUM3=$((VNUM3+1))
-else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-  exit 1
-fi
+
 
 # create new tag
-NEW_TAG=$VNUM1.$VNUM2.$VNUM3
+NEW_TAG=$VNUM1
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
